de9dac29d79c6c0c4a6c74736214d6ed963fbf02
Thiago Peixoto <thiagopeixoto81xd@gmail.com>
2022-12-27 16:28:43 -0300

    ajustando a rota

--- MovieSearcher/src/App.tsx
+++ MovieSearcher/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/home' element={<MovieSearcher />} />
+        <Route path='/' element={<MovieSearcher />} />
       </Routes>
     </>
 
