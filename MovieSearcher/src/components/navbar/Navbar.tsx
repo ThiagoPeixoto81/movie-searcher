d195a47d9108c3ac14c6267a3c59641c15a9220a
Thiago Peixoto <thiagopeixoto81xd@gmail.com>
2023-01-03 17:13:34 -0300

    logo

--- MovieSearcher/src/components/navbar/Navbar.tsx
+++ MovieSearcher/src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
-import { FilmSlate, MagnifyingGlass } from 'phosphor-react'
+import { MagnifyingGlass } from 'phosphor-react'
 import React, { KeyboardEvent } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import LogoMs from './logoMS.svg'
 import InputSearch from '../InputSearch/InputSearch'
 import './navbar.css'
 
@@ -15,7 +16,7 @@ export default function Navbar({ search, setsearch, searchMovie }: INavbar) {
     return (
         <nav className="navbar px-4 py-4 mt-4" >
             <div className="container-fluid d-flex justify-content-spacebetween align-items-center" >
-                <Link to='/'><FilmSlate></FilmSlate></Link>
+                <Link to='/'><img className='navbar-brand' src={LogoMs} alt="Logo Movie Searcher" /></Link>
                 <InputSearch searchMovie={searchMovie} setsearch={setsearch} search={search} />
             </div >
         </nav >
