d472b1509dad5c02fca7d933f2124741baaf3958
Thiago Peixoto <thiagopeixoto81xd@gmail.com>
2023-01-03 17:15:59 -0300

    limpando o repositorio

--- MovieSearcher/src/App.tsx
+++ MovieSearcher/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import MovieSearcher from './pages/MovieSearcher'
 
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
